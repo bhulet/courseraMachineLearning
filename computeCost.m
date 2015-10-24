c0146ba1a2e2d1eb86d457a872746ac991a8eefb
Ben <bhulet@users.noreply.github.com>
2015-10-23 17:34:22 -0700

    Create computeCost.m

---
+++ computeCost.m
@@ -0,0 +1,9 @@
+% Compute the cost functin for linear regression
+function J = computeCost(X, y, theta)
+m = length(y); % number of training examples
+predictions = X*theta;
+sqrErrors = (predictions - y).^2
+J = 0;
+J = 1/(2*m) * sum(sqrErrors);
+
+end
