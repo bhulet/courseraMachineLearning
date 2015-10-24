b606d3629bfb2f212846390e059e2f8188ffacfe
Ben <bhulet@users.noreply.github.com>
2015-10-24 16:33:25 -0700

    Update sigmoid.m

--- sigmoid.m
+++ sigmoid.m
@@ -1,10 +1,6 @@
 function g = sigmoid(z)
-g = zeros(size(z));
-for i = 1:size(z, 1);
-    for j = 1:size(z, 2);
-        g(i,j) = 1 / (1 + exp( -z(i,j) ) );
-    end;
-end;
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
 
-
+g = 1.0 ./ (1.0 + exp(-z));
 end
